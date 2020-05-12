16e0bfaec42058e4805caf416f3aae2e18aba0c9
Lirimi <58885067+Lirimi@users.noreply.github.com>
2020-05-12 18:59:31 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/lumprv/PI_Gr19""
    This reverts commit 2534d3c50e66f1fe2bc77e38989e620427b2eeee.

--- html/stamboll.php
+++ html/stamboll.php
@@ -102,7 +102,7 @@
                         <li><a href="Kontakti.php">REZERVO</a></li>
                         <li><a href="#">FUN</a>
                             <ul>
-                                <li><a href="Chooselanguage.php">LANGUAGE</a></li>
+                               
                                 <li><a href="game.php">GAME</a></li>
                             </ul>
                         </li>
@@ -437,100 +437,6 @@
                     </span>
             </div>
         </div>
-        <!-- <footer>
-            <div class="footer-main">
-                <div class="footer-inner">
-                    <div class="footer-left">
-                        <div class="footer-box">
-
-                            <h6>ABOUT US</h6>
-                            <img src="../images/blonde.jpg" width="215px" height="90px" />
-
-
-                            <p> JOLILU travel është një udhëzues gjithëpërfshirës për destinacionet
-                                më të mira të udhëtimit në botë.Ne ofrojmë përmbajtje të detajuar
-                                dhe të saktë të udhëtimit, të dizajnuara për të frymëzuar udhëtarët globalë.</p>
-                            <a style="float: left" href="Sherbimet.php">Read more &raquo</a>
-                        </div>
-                    </div>
-                    <div class="footer-left">
-                        <div class="footer-box">
-                            <h6>LINK BLOCK</h6>
-                            <ul>
-                                <li style="border-bottom: 1px solid #919090"><a href="https://aria-travel.com/"
-                                        target="_blank">&raquo
-                                        AriaTravelAlbania</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a href="https://travel.com/"
-                                        target="_blank">&raquo
-                                        Travel</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a href="https://www.flyfrontier.com/"
-                                        target="_blank">&raquo
-                                        FlyFrontier</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a
-                                        href="https://www.skyscanner.co.in/airlines-all.php" target="_blank">&raquo
-                                        AirlineInformation</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a href="https://www.expedia.com/Flights"
-                                        target="_blank">&raquo
-                                        CheapFlights</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a href="https://www.flyksa.com/Fr/"
-                                        target="_blank">&raquo
-                                        KosovaAirline</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a href="https://wizzair.com/en-gb"
-                                        target="_blank">&raquo
-                                        Wizzair</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a
-                                        href="https://www.booking.com/index.en.php?aid=1725925;label=en-hc-s4glC*wNOeGEZ7B_fjY56gS336411372683:pl:ta:p1:p2:ac:ap1t1:neg:fi:tikwd-29738972:lp9070165:li:dec:dm;ws=&gclid=Cj0KCQiAr8bwBRD4ARIsAHa4YyLVGbHhMLxOMak9qpd0ujKJ9cfnMF8B3yjazKJZ-lNSBOCK3-zLs8gaAuC-EALw_wcB"
-                                        target="_blank">&raquo
-                                        Hotels and More</a>
-                                </li>
-                                <li style="border-bottom: 1px solid #919090"><a href="../README.md"
-                                        target="_blank">&raquo Readme</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="footer-left">
-                        <div class="footer-box">
-                            <h6>FROM THE BLOG</h6>
-                            <h4>7 Vendet më të vizituara këtë vit..</h4>
-
-                            <p>Spanja ka qenë gjithmonë destinacion i preferuar për shumëkënd,
-                                dhe ende mbetet një nga vendet..
-                                <a style="float: left" href="lavdatat.php">Read more &raquo</a>
-                                <br>
-                                <br>
-
-                                <div class="latestb">
-                                    <h4>Vizitë në Britani</h4>
-                                    <p>Asnjë listë destinacionesh pushimi nuk do ishte e plotë pa përfshirë
-                                        aty Britaninë e Madhe: jo vetëm Anglinë, por edhe Skocinë...</p>
-                                    <a style="float: left" href="lavdata2.php">Read more &raquo</a>
-                                </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="footer-left">
-                    <div class="footer-box">
-                        <h6 id="kontakti">CONTACT US</h6>
-                        <form id="contact-form" method="post" action="" autocomplete="on">
-                            <input type="text" id="name" name="name" placeholder="Name" class="required">
-                            <input type="text" id="email" name="email" placeholder="Email" class="required">
-                            <textarea name="message" placeholder="Message" rows="6" class="required"></textarea><br>
-                            <input type="submit" name="SUBMIT" value="SUBMIT">
-                        </form>
-                    </div>
-                </div>
-                <div class="clr"></div>
-            </div>
-
-        </footer> -->
 
         <?php
 
