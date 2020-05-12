89f7d49136d4b2459b7b53623592bc391260ba5b
Lirimi <58885067+Lirimi@users.noreply.github.com>
2020-05-12 18:19:31 +0200

    Revert "Update files"
    This reverts commit 3f98c646ebc617e6eceb92b8a3b8b4092231850f.

--- html/Sherbimet.php
+++ html/Sherbimet.php
@@ -224,7 +224,7 @@
             <li><a href="Kontakti.php">REZERVO</a></li>
             <li><a href="#">FUN</a>
               <ul>
-               
+                <li><a href="Chooselanguage.php">LANGUAGE</a></li>
                 <li><a href="game.php">GAME</a></li>
               </ul>
             </li>
@@ -237,8 +237,8 @@
     </div>
 
     <div class="wrapper col3">
-      <!--<br><button style="float:left; margin-right: 20px; margin-left: 20px;" class="btn1">Fsheh Sherbimet</button>
-      <button style="float:left" class="btn2">Shfaq Sherbimet</button></br>-->
+      <br><button style="float:left; margin-right: 20px; margin-left: 20px;" class="btn1">Fsheh Sherbimet</button>
+      <button style="float:left" class="btn2">Shfaq Sherbimet</button></br>
       <div id="container" class="clear">
         <!-- main content -->
         <div id="homepage">
@@ -388,11 +388,98 @@
 
 
 
-      <?php
+    <footer>
+      <div class="footer-main">
+        <div class="footer-inner">
+          <div class="footer-left">
+            <div class="footer-box">
 
-      include "footer.php";
+              <h6>ABOUT US</h6>
+              <img src="../images/372.jpg" width="215px" height="90px" />
 
-      ?>
+
+              <p> JOLILU travel është një udhëzues gjithëpërfshirës për destinacionet
+                më të mira të udhëtimit në botë.Ne ofrojmë përmbajtje të detajuar
+                dhe të saktë të udhëtimit, të dizajnuara për të frymëzuar udhëtarët globalë.</p>
+              <a style="float: left" href="Sherbimet.php">Read more &raquo</a>
+            </div>
+          </div>
+          <div class="footer-left">
+            <div class="footer-box">
+              <h6>LINK BLOCK</h6>
+              <ul>
+                <li style="border-bottom: 1px solid #919090"><a href="https://aria-travel.com/" target="_blank">&raquo
+                    AriaTravelAlbania</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a href="https://travel.com/" target="_blank">&raquo
+                    Travel</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a href="https://www.flyfrontier.com/"
+                    target="_blank">&raquo
+                    FlyFrontier</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a href="https://www.skyscanner.co.in/airlines-all.php"
+                    target="_blank">&raquo AirlineInformation</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a href="https://www.expedia.com/Flights"
+                    target="_blank">&raquo
+                    CheapFlights</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a href="https://www.flyksa.com/Fr/" target="_blank">&raquo
+                    KosovaAirline</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a href="https://wizzair.com/en-gb" target="_blank">&raquo
+                    Wizzair</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a
+                    href="https://www.booking.com/index.en.php?aid=1725925;label=en-hc-s4glC*wNOeGEZ7B_fjY56gS336411372683:pl:ta:p1:p2:ac:ap1t1:neg:fi:tikwd-29738972:lp9070165:li:dec:dm;ws=&gclid=Cj0KCQiAr8bwBRD4ARIsAHa4YyLVGbHhMLxOMak9qpd0ujKJ9cfnMF8B3yjazKJZ-lNSBOCK3-zLs8gaAuC-EALw_wcB"
+                    target="_blank">&raquo
+                    Hotels and More</a>
+                </li>
+                <li style="border-bottom: 1px solid #919090"><a href="../README.md" target="_blank">&raquo Readme</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="footer-left">
+            <div class="footer-box">
+              <h6>ME TE REJAT</h6>
+
+              <h4>Dubai - 6 ditë - €629</h4>
+
+              <p>Dita e Parë: Nisja me Autobus nga Prishtina në Aeroportin e Dubait.It was popularised in the 1960s with
+                the release of </p>
+              <a style="float: left" href="lavdatat.php">Read more &raquo</a>
+              <br>
+
+
+              <div class="latestb">
+                <h4>Barcelona - 6 ditë - €829</h4>
+
+                <p>Dita e Parë: Nisja me Autobus nga Prishtina në Aeroportin e Shkupit . It was popularised in the 1960s
+                  with the release otaining</p>
+                <a style="float: left" href="lavdata3.php">Read more &raquo</a>
+                <br>
+                <br>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-left">
+          <div class="footer-box">
+            <h6 id="kontakti">CONTACT US</h6>
+            <form id="contact-form" method="post" action="" autocomplete="on">
+              <input type="text" id="name" name="name" placeholder="Name" class="required">
+              <input type="text" id="email" name="email" placeholder="Email" class="required">
+              <textarea name="message" placeholder="Message" rows="6" class="required"></textarea><br>
+              <input type="submit" name="SUBMIT" value="SUBMIT">
+            </form>
+          </div>
+        </div>
+        <div class="clr"></div>
+      </div>
+    </footer>
   </div>
   <script src="../js/backtotop.js"></script>
   <script src="../js/rregullat.js"></script>
