5ff8ddf7df55b02723f106e620377c1f52a153f6
Lum Pireva <pirevalum@gmail.com>
2020-05-13 03:39:17 +0200

    Register Form
    Nje Register Form e marrur nga Colorlib e punuar ne Bootstrap

--- html/login/register.php
+++ html/login/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign Up Form by Colorlib</title>
+    <title>Sign Up</title>
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
@@ -17,27 +17,26 @@
     <div class="main">
 
         <section class="signup">
-            <!-- <img src="images/signup-bg.jpg" alt=""> -->
             <div class="container">
                 <div class="signup-content">
                     <form method="POST" id="signup-form" class="signup-form">
-                        <h2 class="form-title">Login</h2>
+                        <h2 class="form-title">Create account</h2>
                         <div class="form-group">
                             <input type="text" class="form-input" name="name" id="name" placeholder="Your Username"/>
                         </div>
-                        
+                        <div class="form-group">
+                            <input type="email" class="form-input" name="email" id="email" placeholder="Your Email"/>
+                        </div>
                         <div class="form-group">
                             <input type="text" class="form-input" name="password" id="password" placeholder="Password"/>
                             <span toggle="#password" class="zmdi zmdi-eye field-icon toggle-password"></span>
-                        </div>
-                        
-                       
+                        </div>   
                         <div class="form-group">
-                            <input type="submit" name="submit" id="submit" class="form-submit" value="Log In"/>
+                            <input type="submit" name="submit" id="submit" class="form-submit" value="Sign up"/>
                         </div>
                     </form>
                     <p class="loginhere">
-                        Are you a new member ? <a href="register.html" class="loginhere-link">Register here</a>
+                        Have already an account ? <a href="login.php" class="loginhere-link">Login here</a>
                     </p>
                 </div>
             </div>
