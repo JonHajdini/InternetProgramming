a8f55dbc84b3b85fd7c4ac26be4ad1480548da78
JonHajdini <jon_hajdini@hotmail.com>
2020-05-13 19:32:49 +0200

    Perodrimi i RegEx dhe strlen tek footer.php
    preg_match

--- html/footer.php
+++ html/footer.php
@@ -87,20 +87,142 @@
                         </div>
                     </div>
                 </div>
-                <div class="footer-left">
-                    <div class="footer-box">
-                        <h6 id="kontakti">CONTACT US</h6>
-                        <form id="contact-form" method="post" action="" autocomplete="on">
-                            <input type="text" id="name" name="name" placeholder="Name" class="required">
-                            <input type="text" id="email" name="email" placeholder="Email" class="required">
-                            <textarea name="message" placeholder="Message" rows="6" class="required"></textarea><br>
-                            <input type="submit" name="SUBMIT" value="SUBMIT">
-                        </form>
-                    </div>
-                </div>
-                <div class="clr"></div>
-            </div>
+        <?php
+        if(isset($_POST['email'])) {
+            $email_to = "jon_hajdini@hotmail.com";
+            $email_subject = "Summarized propose of the email";
+//Errors to show if there is a problem in form fields.
+            function died($error) {
+
+                echo "Na vjen keq që ne s'mund të ndjekim kërkesën tuaj për shkak të gabimit,ose mos permbushjes te keresave
+                te te dhenave tuaja.<br /><br />";
+                echo "Below is the error(s) list <br /><br />";
+                echo $error."<br /><br />";
+                echo "Ju lutem shtypni butonin Back per t'i rishkruar te dhenat  e juaja: <br><br/>>";
+                die();
+            }
+// validation expected data exists
+            if(!isset($_POST['first_name']) ||
+                !isset($_POST['last_name']) ||
+                !isset($_POST['email']) ||
+                !isset($_POST['telephone']) ||
+                !isset($_POST['comments'])) {
+                died('We are sorry to proceed your request due to error within form entries');
+            }
+            $first_name = $_POST['first_name']; // required
+            $last_name = $_POST['last_name']; // required
+            $email_from = $_POST['email']; // required
+            $telephone = $_POST['telephone']; // not required
+            $comments = $_POST['comments']; // required
+            $error_message = "";
+            $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+            //--------------------------------preg_match function----------------------------------------------------------------
+            if(!preg_match($email_exp,$email_from)) {
+
+                $error_message .= 'Ju keni shenuar email jo-valide!<br />';
+            }
+            $string_exp = "/^[A-Za-z .'-]+$/";
+            if(!preg_match($string_exp,$first_name)) {
+                $error_message .= 'Emri juaj eshte jo-valid<br />';
+            }
+            if(!preg_match($string_exp,$last_name)) {
+
+                $error_message .= 'Mbiemri juaj eshte jo-valid<br />';
+            }
+
+
+            //-------------------------------- strlen function -----------------------------------------------------------------
+
+            if(strlen($comments) < 2) {
+
+                $error_message .= 'Komenti juaj eshte jo-valid<br />';
+            }
+            if(strlen($error_message) > 0) {
+                died($error_message);
+            }
+            $email_message = "Form details below.\n\n";
+            function clean_string($string) {
+                $bad = array("content-type","bcc:","to:","cc:","href");
+                return str_replace($bad,"",$string);
+            }
+            $email_message .= "First Name: ".clean_string($first_name)."\n";
+            $email_message .= "Last Name: ".clean_string($last_name)."\n";
+            $email_message .= "Email: ".clean_string($email_from)."\n";
+            $email_message .= "Telephone: ".clean_string($telephone)."\n";
+            $email_message .= "Comments: ".clean_string($comments)."\n";
+// create email headers
+            $headers = 'From: '.$email_from."\r\n".
+                'Reply-To: '.$email_from."\r\n" .
+                'X-Mailer: PHP/' . phpversion();
+            @mail($email_to, $email_subject, $email_message, $headers);
+
+            ?>
+            <!-- include your own success html here -->
+
+
+            <?php
+            echo '<script type="text/javascript">';
+            echo ' alert("Ju faleminderit qe kontaktuat me Ne")';  //not showing an alert box.
+            echo '</script>';
+        }
+        ?>
+        <br>
+        <h6 id="kontakti">CONTACT US</h6>
+        <form name="contactform" method="post" action="footer.php">
+            <table width="450px">
+                <tr>
+                    <td valign="top">
+                        <label for="first_name">Emri</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="first_name" maxlength="50" size="30">
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top"">
+                    <label for="last_name">Mbiemri</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="last_name" maxlength="50" size="30">
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top">
+                        <label for="email">Email Addressa</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="email" maxlength="80" size="30">
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top">
+                        <label for="telephone">Numri i Telefonit</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="telephone" maxlength="30" size="30">
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top">
+                        <label for="comments">Komenti juaj</label>
+                    </td>
+                    <td valign="top">
+                        <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="text-align:center">
+                        <input type="submit" value="Submit">  </a>
+                    </td>
+                </tr>
+            </table>
+
     </footer>
+
+
+
+
 
 </body>
 </html>
