6420cabcf3b4408268a4d02283ad9c2bf995f19d
Lum Pireva <pirevalum@gmail.com>
2020-05-14 18:48:24 +0200

    Update Kategorite ne admin
    Ofrimi per ti bere update kategorite, si dhe rregullimi i kodit duke krijuar nje file 'functions.php'

--- html/admin/categories.php
+++ html/admin/categories.php
@@ -17,37 +17,11 @@
                             Welcome to admin
                             <small>Author</small>
                         </h1>
-                       
-                       
+                                           
                         <div class="col-xs-6">
- <?php
 
-if(isset($_POST['submit'])){
+                        <?php insert_categories(); ?>
 
-    $kat_emri = $_POST['cat_title'];
-
-    if($kat_emri == "" || empty($kat_emri)){
-
-        echo "This field should not be empty";
-
-    } else {
-
-        $query = "INSERT INTO kategorite(kat_emri) ";
-        $query .= "VALUE('{$kat_emri}')" ;
-
-        $create_category_query = mysqli_query($connection, $query);
-
-        if(!$create_category_query){
-
-            die('QUERY FAILED' . mysqli_error($connection));
-
-        }
-
-    }
-
-}
-
-?>
                             <form action="" method="post">
                                 <div class="form-group">
                                     <label for="cat-title">Add Category</label>
@@ -57,13 +31,23 @@ if(isset($_POST['submit'])){
                                     <input class="btn btn-primary" type="submit" name="submit" value="Add Category"> 
                                 </div>
 
-                            </form>       
+                            </form>      
+
+                    <?php
+                    
+                    if(isset($_GET['edit'])){
+
+                        $kat_id = $_GET['edit'];
+                        
+                        include "includes/update_categories.php";
+                    }
+
+                    ?>
+
                     </div>
 
                     <div class="col-xs-6">
  
-                    
-
                     <table class="table table-bordered table-hover">
                         <thead>
                             <tr>
@@ -73,42 +57,9 @@ if(isset($_POST['submit'])){
                         </thead>
                         <tbody>
 
-                    <?php 
- 
+                            <?php  findAllCategories(); ?>
 
-                    // Marrja e informatave nga tabela kategorite 
-                    $query = "SELECT * FROM kategorite";
-                    $select_kategorite = mysqli_query($connection, $query);
-
-
-                    while($row = mysqli_fetch_assoc($select_kategorite)){
-                        
-                        $kat_id = $row['kat_id'];
-                        $kat_emri = $row['kat_emri'];
-
-                        echo "<tr>";
-                        echo "<td>{$kat_id}</td>";
-                        echo "<td>{$kat_emri}</td>";
-                        echo "<td><a href='categories.php?delete={$kat_id}'>Delete</a></td>";
-                        echo "</tr>";
-
-                    }
-
-                    ?>
-
-                    <?php 
-
-                    if(isset($_GET['delete'])){
-
-                        $get_kat_id = $_GET['delete']; 
-                        
-                        $query = "DELETE FROM kategorite WHERE kat_id = {$get_kat_id} ";
-                        $delete_query = mysqli_query($connection, $query);
-
-                        header("Location: categories.php"); //refresh page after delete
-                    }
-
-                    ?>
+                            <?php deleteCategories(); ?>
 
                         </tbody>
                     </table>
---
+++ html/admin/functions.php
@@ -0,0 +1,69 @@
+<?php
+function insert_categories(){
+    global $connection;
+
+    if(isset($_POST['submit'])){
+
+        $kat_emri = $_POST['cat_title'];
+
+        if($kat_emri == "" || empty($kat_emri)){
+
+            echo "This field should not be empty";
+
+        } else {
+
+            $query = "INSERT INTO kategorite(kat_emri) ";
+            $query .= "VALUE('{$kat_emri}')" ;
+
+            $create_category_query = mysqli_query($connection, $query);
+
+            if(!$create_category_query){
+
+                die('QUERY FAILED' . mysqli_error($connection));
+
+            }
+
+        }
+
+    }
+}
+
+
+function findAllCategories(){
+    global $connection;
+
+    $query = "SELECT * FROM kategorite";
+    $select_kategorite = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_kategorite)){
+                        
+        $kat_id = $row['kat_id'];
+        $kat_emri = $row['kat_emri'];
+
+        echo "<tr>";
+        echo "<td>{$kat_id}</td>";
+        echo "<td>{$kat_emri}</td>";
+        echo "<td><a href='categories.php?delete={$kat_id}'>Delete</a></td>";
+        echo "<td><a href='categories.php?edit={$kat_id}'>Edit</a></td>";
+        echo "</tr>";
+
+    }
+
+}
+
+
+function deleteCategories(){
+    global $connection;
+
+    if(isset($_GET['delete'])){
+
+        $get_kat_id = $_GET['delete']; 
+        
+        $query = "DELETE FROM kategorite WHERE kat_id = {$get_kat_id} ";
+        $delete_query = mysqli_query($connection, $query);
+
+        header("Location: categories.php"); 
+    }
+}
+
+?>
