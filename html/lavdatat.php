305e02dfec11c207d28f92a8b9694c95b7199dc7
JonHajdini <jon_hajdini@hotmail.com>
2020-05-14 03:37:30 +0200

    Update tek lavdata.php

--- html/lavdatat.php
+++ html/lavdatat.php
@@ -51,11 +51,36 @@
                     <div class="three_quarter">
                         <blockquote>
                             <p style="font-size: 24px;"><br>
-                                Dita e Parë: Nisja me Autobus nga Prishtina në Aeroportin e Shkupit. Fluturimi Shkup –
+                               <?php $text = "Dita e Parë: Nisja me Áutobus nga Prishtina në Áeroportin e Shkupit. Fluturimi Shkup –
                                 Dubai. Paraqitja ne Check in 2 ore para nisjes dhe njohja me Guiden. Nisja nga Shkupi në
-                                orën 13.35, Mbrritja në Dubai në orën 20.35. Transferta me Autobus nga Aeroporti në
-                                Hotel. Sistemimi dhe Fjetje në Hotel.
-                                </br>
+                                orën 13.35, Mbrritja në Dubai në orën 20.35. Transferta me Áutobus nga Aeroporti në
+                                Hotel. Sistemimi dhe Fjetje në Hotel.\n";
+
+                               //--------------------preg_replace function------------------------------------------
+                            function remove_accent($text)
+                            {
+                                $a = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 'ǿ');
+                                $b = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'l', 'l', 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o');
+                                return str_replace($a, $b, $text);
+
+                            }
+
+                            function post_slug($text)
+                            {
+                                return (preg_replace(array('/[^a-zA-Z0-9 -,.:]/', '/[ -]+/', '/^-|-$/'),
+                                    array('', ' ', ''), remove_accent($text)));
+                            }
+                                   echo post_slug($text);
+                            //------------------------preg_split-------------------------------------------------
+                                   $result = preg_split("/[\n]+/", "--Hotel Maria, \n --Hotel Aria, \n --Hotel Parajsa, ");
+                                   echo "<br>";
+                                   // Display result
+                                   foreach($result as $a)
+                                   {
+                                       print $a;
+                                   }
+                                   ?>
+
                                 <br />
                                 <br />
                                 Dita e Dytë: Mëngjesi në Hotel. Një vizitë panoramike në Dubai ku vizitohen: Pallati e
@@ -123,7 +148,7 @@
                                 <br>
                                 Nisja më 18 Maj.
                                 </br>
-                            </p>
+                            </>
                         </blockquote>
 
                     </div>
