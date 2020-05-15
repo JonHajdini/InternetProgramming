926a90eb68f5d9c4a2e2d2874db726c6babf3bee
Lum Pireva <pirevalum@gmail.com>
2020-05-15 19:08:07 +0200

    Rregullimi i passwordit
    Mundesia per tu kyqur useri i regjistruar me password te enkriptuar me heret

--- html/login/login.php
+++ html/login/login.php
@@ -28,6 +28,8 @@ if(isset($_POST['login'])){
         $db_user_role = $row['user_role'];
     }
 
+    $password = crypt($password, $db_user_password);
+
     if($username !== $db_username && $password !== $db_user_password){
         header("Location: login.php");
     } else if($username == $db_username && $password == $db_user_password){
